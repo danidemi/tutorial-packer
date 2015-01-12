0ea6e0ad59f50522a1cbc78aa0b8f7e3a8259352
danidemi <demichelis@danidemi.com>
2015-01-12 08:33:42 +0100

    files renamed

--- tutorial-webapp-tomcat-docker/docker/tomcat/docker_app_in_memory.sh
+++ tutorial-webapp-tomcat-docker/docker/tomcat/docker_app_in_memory.sh
@@ -1,15 +1,16 @@
 # build and run a dockerized image of the app that needs a database
 rm *.war 
 pushd ../../
-mvn clean install
+mvn clean install -Dspecific -Pdockerized.memory
 popd
-cp ../../target/tutorial-webapp-tomcat-docker-*.war .
+cp ../../target/tutorial-webapp-tomcat-docker-*.war ./items.war
 
 # rm previous image
-docker rmi "itemsapp:0.1"
+docker rmi -f "itemsapp:0.1"
 
 # create image
 docker build --tag="itemsapp:0.1" .
 
 # create and run application container
-docker run --name="itemsapp" -P itemsapp:0.1 "myself/mytomcat:0.1"
+docker rm "itemsapp"
+docker run --name="itemsapp" -P itemsapp:0.1
